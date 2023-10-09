15578d9aafdfc487fb99da74851f83b09d68f1fa
moogleKupo <tmaine@gmx.com>
2023-10-09 18:40:31 +0000

    prevent values in db from being null

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,41 +1,66 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
+
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
+  id SERIAL PRIMARY KEY,
+@@ -9,36 +12,36 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description TEXT,
-    costpernight DECIMAL(10, 2) NOT NULL,
-    parking_spaces INTEGER,
-    numberofbathrooms INTEGER,
-    numberofbedrooms INTEGER,
-    thumbnail_photo_url VARCHAR(255),
-    cover_photo_url VARCHAR(255),
-    country VARCHAR(255),
-    street VARCHAR(255),
-    city VARCHAR(255),
-    province VARCHAR(255),
-    post_code VARCHAR(10),
-    is_active BOOLEAN,
-    owner_id INTEGER REFERENCES users(id)
+  id SERIAL PRIMARY KEY,
+  owner_user_id INT REFERENCES users(id),
+  title VARCHAR(255),
+  owner_user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  cost_per_night INT,
+  number_of_parking_spaces INT,
+  number_of_bathrooms INT,
+  number_of_bedrooms INT,
+  thumbnail_url VARCHAR(255),
+  cover_url VARCHAR(255),
+  country VARCHAR(255),
+  province VARCHAR(225),
+  city VARCHAR(225),
+  street VARCHAR(225),
+  postal_code VARCHAR(225),
+  active BOOLEAN
+  cost_per_night INT NOT NULL DEFAULT 0,
+  number_of_parking_spaces INT NOT NULL DEFAULT 0,
+  number_of_bathrooms INT NOT NULL DEFAULT 0,
+  number_of_bedrooms INT NOT NULL DEFAULT 0,
+  thumbnail_url VARCHAR(255) NOT NULL,
+  cover_url VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  province VARCHAR(225) NOT NULL,
+  city VARCHAR(225) NOT NULL,
+  street VARCHAR(225) NOT NULL,
+  postal_code VARCHAR(225) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE reservations (
-    id SERIAL PRIMARY KEY,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    guest_id INTEGER REFERENCES users(id),
-    property_id INTEGER REFERENCES properties(id)
+  id SERIAL PRIMARY KEY,
+  start_date DATE,
+  end_date DATE,
+  guest_user_id INT REFERENCES users(id),
+  property_id INT REFERENCES properties(id)
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  guest_user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  property_id INT REFERENCES properties(id) ON DELETE CASCADE
 );
 
 CREATE TABLE property_reviews (
-    id SERIAL PRIMARY KEY,
-    message TEXT,
-    rating INTEGER CHECK (rating >= 1 AND rating <= 5),
-    guest_id INTEGER REFERENCES users(id),
-    reservation_id INTEGER REFERENCES reservations(id)
+  id SERIAL PRIMARY KEY,
+  message TEXT,
+  rating SMALLINT,
+  guest_user_id INT REFERENCES users(id),
+  reservation_id INT REFERENCES reservations(id),
+  property_id INT REFERENCES properties(id)
+  rating SMALLINT NOT NULL DEFAULT 0,
+  guest_user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INT REFERENCES reservations(id) ON DELETE CASCADE,
+  property_id INT REFERENCES properties(id) ON DELETE CASCADE
 );
