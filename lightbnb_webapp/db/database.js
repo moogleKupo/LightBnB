24ed4cc74d30741fabc3421db992da087d824898
moogleKupo <tmaine@gmx.com>
2023-10-12 22:49:07 +0000

    fix addProperty function

--- lightbnb_webapp/db/database.js
+++ lightbnb_webapp/db/database.js
@@ -150,17 +150,39 @@ const getAllProperties = function (options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = (property) => {
-  return pool
-    .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $12, $13, $14) RETURNING *;`, [property])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
+const addProperty = function (property) {
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+    RETURNING *;
+  `;
+
+  return pool.query(queryString, queryParams)
+    .then((res) => {
+      return res.rows[0]; // Return the saved property
     })
     .catch((err) => {
-      console.log(err.message);
+      console.error(err.message);
     });
 };
+
 
 module.exports = {
   getUserWithEmail,
